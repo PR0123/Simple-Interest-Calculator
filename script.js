fdfe0cbd5fb0c003e72bee8a619e0399952d982a
PR0123 <81814529+PR0123@users.noreply.github.com>
2021-04-06 01:09:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-var principal = document.getElementById("principal").value ?? 0;
-var rate = document.getElementById("rate").value;
-var years = document.getElementById("years").value;
-var interest = principal * years * rate / 100
 
 
 function compute()
 {
     p = document.getElementById("principal").value;
+    
+    principal = document.getElementById("principal").value ?? 0;
+    rate = document.getElementById("rate").value;
+    years = document.getElementById("years").value;
+    interest = principal * years * rate / 100
+
+    
     
 }
 
