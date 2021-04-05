ee45afbd4f8fa529c0dfd3e330cfb561ed11e389
PR0123 <81814529+PR0123@users.noreply.github.com>
2021-04-06 00:53:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+var principal = document.getElementById("principal").value;
+var rate = document.getElementById("rate").value;
+var years = document.getElementById("years").value;
+var interest = principal * years * rate / 100
+
+
 function compute()
 {
     p = document.getElementById("principal").value;
     
 }
-        
+
+function readSlider()
+{
+    p = document.getElementById("rate").value;
+    document.getElementById("display").value = p
+
+}
+
