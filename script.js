3bc20d3d0edc6d5aa6922c5a7d5fcc580e1b6122
anonymous <this.is@not.valid.email>
2021-04-06 17:00:01 +0000

    bug fixes
    Signed-off-by: anonymous <this.is@not.valid.email>

--- script.js
+++ script.js
@@ -1,43 +1,36 @@
 
-
+//the button in html file has the onclick attribute set to "compute()"
 function compute()
 {
     var p = document.getElementById("principal")
     var principal = p.value
-    
-
+    // If the user enters zero or negative values, alert "Enter a positive number"
     if(principal <= 0){ 
         alert("Enter positive number"); 
         p.focus();
         return false;
     }
-
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate / 100
-
-
-
+    var yearsInt = parseInt(years) + 2021
     var result = document.getElementById("result")
-    result.innerHTML = 
-    "If you deposit" 
-    + principal 
-    + ",at an interest rate of " 
-    +  rate
-    +"%.You will receive an amount of " 
-    + interest
-    + ",in the year" 
-    + 2021+years
 
-    
-    
+    result.innerHTML = 
+    "<br/><b>If</b> you deposit " 
+    + principal 
+    + ",<br/><b>at</b> an interest rate of " 
+    +  rate
+    +"%.<br/><b>You</b> will receive an amount of " 
+    + interest
+    + ",<br/><b>in</b> the year " 
+    + yearsInt
 }
 
-
+//function that reads the value of the range slider and displays it the <span>adjacent to the slider.
 function readSlider()
 {
     p = document.getElementById("rate").value;
-    document.getElementById("display").innerHTML = p;
-
+    document.getElementById("display").innerHTML = p+"%";
 }
 
