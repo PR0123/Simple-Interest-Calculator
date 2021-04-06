9071fcedc09b994e8cac829aad0143b6ff8905d9
anonymous <this.is@not.valid.email>
2021-04-06 15:52:37 +0000

    applying style
    Signed-off-by: anonymous <this.is@not.valid.email>

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 
-/*
+
 function compute()
 {
-    principal = document.getElementById("principal").value ?? 0;
-    rate = document.getElementById("rate").value;
-    years = document.getElementById("years").value;
-    interest = principal * years * rate / 100
+    var principal = document.getElementById("principal").value ?? 0;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100
 
     
     
 }
-*/
+
 
 function readSlider()
 {
