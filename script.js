8e57a1705ce1759adf2c83d7e42c9809d7351839
anonymous <this.is@not.valid.email>
2021-04-06 16:14:33 +0000

    output message
    Signed-off-by: anonymous <this.is@not.valid.email>

--- script.js
+++ script.js
@@ -2,10 +2,24 @@
 
 function compute()
 {
-    var principal = document.getElementById("principal").value ?? 0;
+    var principal = document.getElementById("principal").value
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate / 100
+
+if principal.value <= 0 {
+    alert("Enter a positive number")
+}
+
+    document.getElementById("result").innerHTML = 
+    "If you deposit" 
+    + principal 
+    + ",at an interest rate of " 
+    +  rate
+    +"%.You will receive an amount of " 
+    + interest
+    + ",in the year" 
+    + 2021+years
 
     
     
